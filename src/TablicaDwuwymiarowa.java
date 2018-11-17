b7923993bcd9ff927c4ac5f994f445fddf1b9946
darzak1 <darzak@gmail.com>
2018-11-17 10:34:29 +0100

    po pierwszej przewie

---
+++ src/TablicaDwuwymiarowa.java
@@ -0,0 +1,13 @@
+public class TablicaDwuwymiarowa {
+
+    public static void main(String[] args) {
+
+        boolean area [][] = new boolean[3][3];
+        area [1] [1] = true;
+
+        System.out.println(area [0][0]);
+        System.out.println(area [2][1]);
+        System.out.println(area [1][1]);
+        
+    }
+}
