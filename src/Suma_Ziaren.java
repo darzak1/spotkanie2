4c85379976cd4eb6c920a309968a705ffb49925f
darzak1 <darzak@gmail.com>
2018-12-18 15:23:50 +0100

    po pierwszym zadaniu domowym z tablicą n elementową

---
+++ src/Suma_Ziaren.java
@@ -0,0 +1,18 @@
+public class Suma_Ziaren {
+    public static void main(String[] args) {
+
+
+        int b = 2;
+        int pola[] = new int[64];
+        for (int a = 1; a<=pola.length; a++){
+
+            int potega = (int)Math.pow(a, b);
+
+            System.out.print(potega+ " ");
+        }
+        //System.out.println(pola);
+
+
+
+    }
+}
